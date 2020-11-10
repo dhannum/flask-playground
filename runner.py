4a9133c1457a87cffdd24bdf6eea3a65fe53b616
Daniel Hannum <daniel.hannum@creditkarma.com>
2020-11-10 12:52:37 -0500

    Rip out the cruft

--- runner.py
+++ runner.py
@@ -1,18 +1,9 @@
 import os
-from app import app, db
-from app.models import User, Post, Tag, Category, Employee, Feedback
+from app import app
 from flask_script import Manager, Shell
 from flask_migrate import MigrateCommand
 
 manager = Manager(app)
-
-# these names will be available inside the shell without explicit import
-def make_shell_context():
-    return dict(app=app,  db=db, User=User, Post=Post, Tag=Tag, Category=Category,
-                Employee=Employee, Feedback=Feedback)
-
-manager.add_command('shell', Shell(make_context=make_shell_context))
-manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
